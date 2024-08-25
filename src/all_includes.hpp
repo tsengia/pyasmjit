249f898a5398ff54e0225ef0632b911d492ebefd
Tyler Sengia <tylersengia@gmail.com>
2024-08-25 19:22:38 +0000

    switching to binder generated C++ bindings

---
+++ src/all_includes.hpp
@@ -0,0 +1,8 @@
+#pragma once
+/**
+ * This file is used by binder to find all the header files that need bound to python.
+ */
+#include "asmjit/asmjit.h"
+#include "asmjit/core.h"
+#include "asmjit/x86.h"
+#include "asmjit/arm.h"
