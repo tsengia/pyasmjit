9dd2c3b2a66a7290b172617dbd538cb0c131aaa4
Tyler Sengia <tylersengia@gmail.com>
2024-08-25 16:54:17 +0000

    break C++ code into separate source files

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 
 namespace py = pybind11;
 
+void init_core(py::module_ &);
+void init_x86(py::module_ &);
+
 std::string get_asm_jit_version() {
     return std::to_string(ASMJIT_LIBRARY_VERSION);
 }
@@ -12,4 +15,6 @@ std::string get_asm_jit_version() {
 PYBIND11_MODULE(_core, m) {
     m.def("get_asm_jit_version", &get_asm_jit_version, "Returns the version of asmjit statically compiled into this package.");
 
+    init_core(m);
+    init_x86(m);
 }
---
+++ src/x86.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+#include "pybind11/pybind11.h"
+#include "asmjit/core/api-config.h"
+
+namespace py = pybind11;
+
+void init_x86(py::module_ &) {
+    
+}
